1725c8c04b3d3165c34e85397f0569694a9a1359
Violeta <artnerdnet@gmail.com>
2021-08-07 17:02:56 +0200

    Moved app.js to root

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Palette from "./Palette";
-import "../style/style.scss";
+import Palette from "./components/Palette";
+import "./style/style.scss";
 
 function App() {
   const [palette, setPalette] = useState([]);
